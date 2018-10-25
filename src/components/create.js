b3be62e771dfd1f2be5c3e01de294fdb22be956c
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-10-25 20:02:04 +0300

    Enable adding of tasks directly without use of modal

--- src/components/create.js
+++ src/components/create.js
@@ -2,49 +2,35 @@ import React, {Component} from 'react';
 
 class Create extends Component{
     state  = {
-        value: '',
+        task: '',
     };
 
-    handleChange = (evt) => this.setState({value: evt.target.value});
+    setTask(task) {
+        this.setState({task})
+    }
 
+    handleChange = (evt) => this.setTask(evt.target.value);
+
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        let {task} = this.state;
+        task = task.trim();
+        this.setTask('');
+        if (task !== '') {
+            this.props.onAddTask(task)
+        }
+    };
 
     render() {
-        const {onAddTask} = this.props;
-
         return (
             <>
-                <br/>
-                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
-                    Add
-                </button>
-
-                <div className="modal fade" id="myModal" tabIndex="-1" role="dialog"
-                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalCenterTitle">Add task</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <form>
-                                    <label className={'control-label'}>Task</label>
-                                    <input type={'text'}
-                                           className={'form-control'}
-                                           onChange={this.handleChange}/>
-                                </form>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button"
-                                        className="btn btn-primary"
-                                        onClick={() => onAddTask(this.state.value)}>Add</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
+                    <input type={'text'}
+                           value={this.state.task}
+                           className={'form-control'}
+                           placeholder="Add task"
+                           onChange={this.handleChange}/>
+                </form>
             </>
         )
     }
